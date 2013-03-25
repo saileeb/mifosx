473283d80c1f9f66c4259b77c5a04823d58f88fe
sailee <saileebh@thoughtworks.com>
2013-03-25 18:11:41 +0530

    [Sailee/Khushal]: Included DB setup related commands in travis build files

--- travis_build.sh
+++ travis_build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mysql -uroot -p mifosplatform-tenants < mifosng-db/multi-tenant-demo-backups/0001-mifos-platform-shared-tenants.sql
+mysql -uroot -p mifostenant-default < mifosng-db/multi-tenant-demo-backups/bk_mifostenant-default.sql
 cd mifosng-provider
 gradle clean test
 gradle clean tomcatRunWar

1e16998f0cdc0cdced89534db7bb1a119ede51e7
sailee <saileebh@thoughtworks.com>
2013-03-25 18:43:49 +0530

    [Sailee/Khushal]: Trying DB Setup

--- travis_build.sh
+++ travis_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mysql  mifosplatform-tenants < mifosng-db/multi-tenant-demo-backups/0001-mifos-platform-shared-tenants.sql
-mysql  mifostenant-default < mifosng-db/multi-tenant-demo-backups/bk_mifostenant-default.sql
+mysql  mifosplatform_tenants < mifosng-db/multi-tenant-demo-backups/0001-mifos-platform-shared-tenants.sql
+mysql  mifostenant_default < mifosng-db/multi-tenant-demo-backups/bk_mifostenant-default.sql
 cd mifosng-provider
 gradle clean test
 gradle clean tomcatRunWar
